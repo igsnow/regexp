3a74fdd62221311e9afb072ffb4b09a56cffa412
igsnow <zzy716810918@gmail.com>
2019-11-18 15:56:57 +0800

    æso hard

--- src/create.js
+++ src/create.js
@@ -1,6 +1,14 @@
 // 可以用正则表达式字面量和RegExp的构造函数构建正则
-let re = /ab*c/;                                // *前面字符出现0次或多次
-let re2 = new RegExp('ab+c');           // +前面字符至少出现一次
+{
+    let re = /ab*c/;                                // *前面字符出现0次或多次
+    let re2 = new RegExp('ab+c');           // +前面字符至少出现一次
 
-let str = 'acdefg';
-console.log(str.match(re));
+    let str = 'acdefg';
+    console.log(str.match(re));
+}
+
+{
+
+}
+
+
