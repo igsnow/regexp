63acd9b8abf148dcb37745e953384928e45511b1
igsnow <zzy716810918@gmail.com>
2019-11-19 15:45:50 +0800

    单个字符配合数量规则匹配

---
+++ src/quantifiers.js
@@ -0,0 +1,13 @@
+{
+    let str = 'The Colors of the rainbow have many colours and the rainbow does not  have a single colour.';
+    let re = /colou?rs?/ig;
+    console.log(str.match(re));    // 找出所有的颜色
+}
+
+{
+    let str = "These are some phone numbers 917-555-1234. Also, you can call me at 646.555.1234 and " +
+        "of course I'm always reachable at (212)867-5309.";
+    // 想找出5个字母组成的单词
+    let re = /\b\w{5}\b/g;
+    console.log(str.match(re));
+}
